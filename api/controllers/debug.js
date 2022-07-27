685e16ec0b9e731b482b542d7a48068e288d1263
lilbillybiscuit <qianbill2014@outlook.com>
2022-07-27 03:21:58 -0400

    debug checkpoint

--- api/controllers/debug.js
+++ api/controllers/debug.js
@@ -1,12 +1,14 @@
 exports.get = function (request, result) {
-  console.log(request);
+  console.log(request.params);
+  console.log(request.body);
   result.json({
     success: true,
   })
 }
 
 exports.post = function (request, result) {
-  console.log(request);
+  console.log(request.params);
+  console.log(request.body);
   result.json({
     success: true,
   })
